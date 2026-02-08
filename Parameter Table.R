04b22f9cd4adff1a3c4a9b0a7b7a84059e116387
Mgreinert <matthew.godfrey.reinert@gmail.com>
2026-02-08 21:32:46 +0100

    adding parameter r command

---
+++ Parameter Table.R
@@ -0,0 +1,17 @@
+#Making a parameter table
+#Making a Data Frame with 7 columns matching the variables. "Drug name", "Drug costs", "comparator name", Comparator Cost", "Progression Probability", "Utility Values pf", Utility values p 
+parameter_table <- data.frame(
+  drug_name = c("a", "b", "c"),
+  drug_cost = c(4500, 3000, 3600),
+  comparator_name = c("z","y","X"),
+  comparator_cost = c(3000, 2400, 3300),
+  prog_prob = c(.23, .25, .24),
+  util_pf = c(.86, .87, .82), 
+  util_pp = c(.3, .4, .26)
+)
+#print the parameter table
+parameter_table
+print(parameter_table)
+library(readr)
+write_csv(parameter_table,"C:/Users/Thesq/Documents/Learning R/parameter_table.csv")
+parameter_table[2,2] = 3300

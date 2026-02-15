ac6054553af0b3fab622e4edd9c8d4a8a0d1d50e
Mgreinert <matthew.godfrey.reinert@gmail.com>
2026-02-15 20:51:38 +0100

    Added in a new probability of progression for comparators

--- Parameter Table.R
+++ Parameter Table.R
@@ -5,7 +5,8 @@ parameter_table <- data.frame(
   drug_cost = c(4500, 3000, 3600),
   comparator_name = c("z","y","X"),
   comparator_cost = c(3000, 2400, 3300),
-  prog_prob = c(.23, .25, .24),
+  prog_prob_drug = c(.23, .25, .24),
+  prog_prob_comp = c(.34, .36, .38),
   util_pf = c(.86, .87, .82), 
   util_pp = c(.3, .4, .26)
 )

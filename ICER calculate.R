302e368d0a1bed1ef0523a6b9195c96e6396ae2e
Mgreinert <matthew.godfrey.reinert@gmail.com>
2026-03-09 14:28:47 +0100

    Added two more functions, but I need to figure out how to do nested functions

--- ICER calculate.R
+++ ICER calculate.R
@@ -18,8 +18,30 @@ calculate_qaly_drug_prog <- function(util_pp, prog_prob_drug, patient_count = 1 
 }
 print(calculate_qaly_drug_prog(.3, .23))
 
+#Utility of the patients who didn't progress
+calculate_qaly_drug_pf <- function(util_pf, prog_prob_drug, patient_count = 1 ){
+  pf_patients <- patient_count * (1-prog_prob_drug)
+  pf_patients * util_pf
+}
+print(calculate_qaly_drug_pf(.86, .23))
 
+#Add the QALYS of those who progressed to those who remained progression free together
+
+calculate_qaly_drug <- function(calculate_qaly_drug_pf, calculate_qaly_drug_prog){
+  qaly_drug <- calculate_qaly_drug_pf + calculate_qaly_drug_prog
+}
+print(calculate_qaly_drug)
 
 #2. Calculate the QALY per comparator  
+#inputs: number (util_pf), number (util_pp), number (prog_prob_comp), number (duration)
+#outputs: number (QALY_comp)
+#step 1: multiply 1 (patient #) by prog_prob_comp
+#step 2: Multiply output of step 1 by util_pp
+#step 3: Multiply 1 (patient#) by (1 - prog_prob_comp)
+#Step 4: Multiply result of step 3 by util_pf
+#Step 5: Add the result of Step #2 and Step #4 to get the QALY for the comparator
+
+
+
 #3. Calculate the cost per drug and per comparator
 #4. Convert that into a ICER
